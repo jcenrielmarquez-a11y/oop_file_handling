c78a5a2a09bd5cc913395d3c6f0280d2589dde08
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-04-27 15:57:17 +0800

    Added numbers_separator method that reads integers from the input file, separates them into even and odd lists, writes them to separate files, and includes error handling for missing files or invalid values.

--- seperating_odd_and_even_numbers/numbers_seperator.py
+++ seperating_odd_and_even_numbers/numbers_seperator.py
@@ -2,3 +2,17 @@ class NumbersSeperator:
     def __init__(self, input_file):
         self.input_file = input_file
         self.numbers = []
+
+    def numbers_separator(self, even_file, odd_file):
+        try:
+            with open(self.input_file, "r") as file:
+                self.numbers = [int(line.strip()) for line in file.readlines()]
+
+            self.write_to_file(even_file, self.even_numbers())
+            self.write_to_file(odd_file, self.odd_numbers())
+
+            print("Process Completed")
+        except FileNotFoundError:
+            print("File Not Found")
+        except ValueError:
+            print("Value Error")
