d09615df858126e3822847ba8fa90b4a494145c2
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-04-27 19:52:57 +0800

    Added write_lines method that opens the file in write mode and continuously asks the user for input lines to save into the file.

--- my_life_writer/my_life_writer.py
+++ my_life_writer/my_life_writer.py
@@ -1,4 +1,10 @@
 class MyLifeWriter:
     def __init__(self, file_name):
         self.file_name = file_name
-        
+
+    def write_lines(self):
+        try:
+            with open(self.file_name, "w") as file:
+                while True:
+                    line = input("Enter line: ")
+                    file.write(line + "\n")
