535ff6d3d3b617e4c5dd745f209a057bb1446645
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-04-27 17:03:08 +0800

    Updated read_and_find_highest method to open the file, loop through each line, split student name and GWA using ' - ', format the name properly, and convert the GWA into a float for comparison.

--- highest_gwa/student_highest_gwa.py
+++ highest_gwa/student_highest_gwa.py
@@ -5,4 +5,11 @@ class StudentHighestGwa:
     def read_and_find_highest(self):
         name_with_highest_gwa = ""
         highest_gwa = float("-inf")
-        
+        try:
+            with open(self.file_name, "r") as file:
+                for line in file:
+                    line = line.strip()
+                    if " - " in line:
+                        name, gwa = line.split(" - ")
+                        name = name.strip().title()
+                        gwa = float(gwa.strip())
