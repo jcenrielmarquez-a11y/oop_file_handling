182949e4ff032038a12d690edab8ee49df7fb21e
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-04-27 17:00:54 +0800

    Started read_and_find_highest method by setting up variables to track the student name and the highest GWA, initializing highest_gwa with negative infinity for comparison.

--- highest_gwa/student_highest_gwa.py
+++ highest_gwa/student_highest_gwa.py
@@ -1,3 +1,8 @@
 class StudentHighestGwa:
     def __init__(self, file_name):
         self.file_name = file_name
+
+    def read_and_find_highest(self):
+        name_with_highest_gwa = ""
+        highest_gwa = float("-inf")
+        
