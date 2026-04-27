172a10644349baf9d3d73eb42c54d35509711277
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-04-27 17:12:24 +0800

    Completed read_and_find_highest method by comparing each GWA to track the highest, storing the student’s name, printing the result, and adding error handling for missing files or invalid formats.

--- highest_gwa/student_highest_gwa.py
+++ highest_gwa/student_highest_gwa.py
@@ -5,6 +5,7 @@ class StudentHighestGwa:
     def read_and_find_highest(self):
         name_with_highest_gwa = ""
         highest_gwa = float("-inf")
+
         try:
             with open(self.file_name, "r") as file:
                 for line in file:
@@ -13,3 +14,13 @@ class StudentHighestGwa:
                         name, gwa = line.split(" - ")
                         name = name.strip().title()
                         gwa = float(gwa.strip())
+                        if gwa > highest_gwa:
+                            highest_gwa = gwa
+                            name_with_highest_gwa = name
+
+            print(f"Student with the highest GWA: {name_with_highest_gwa} - {highest_gwa}")
+
+        except FileNotFoundError:
+            print("File not found"))
+        except ValueError:
+            print("Invalid file format"))
