6941c4e4624e51f111af0acfff4e8b7bc5492c41
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-04-29 17:59:26 +0800

    Added process_integers method that opens the input file, reads each line, strips whitespace, converts values to integers, and stores them in a list.

--- square_cube_integers/integers_seperator.py
+++ square_cube_integers/integers_seperator.py
@@ -1,3 +1,10 @@
 class IntegerSeperator:
     def __init__(self, input_file):
         self.input_file = input_file
+
+    def process_integers(self):
+        try:
+            with open(self.input_file, "r") as file:
+                numbers = []
+                for line in file.readlines():
+                    numbers.append(int(line.strip()))
