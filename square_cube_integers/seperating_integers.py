635c9744178955da46bdabbd30d1fe2685dc1023
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-04-29 18:13:52 +0800

    Created main program that imports IntegerSeperator, initializes it with integers.txt, and runs the process_integers method to generate double.txt and triple.txt when executed.

---
+++ square_cube_integers/seperating_integers.py
@@ -0,0 +1,8 @@
+from integers_seperator import IntegerSeperator
+
+def main():
+    handler = IntegerSeperator("integers.txt")
+    handler.process_integers()
+
+if __name__ == "__main__":
+    main()
