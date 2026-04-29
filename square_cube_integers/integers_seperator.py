432eef8e83ffb6b0f7feb2cfd0dde1c08becf641
Jc Enriel Marquez <jcenrielmarquez@gmail.com>
2026-04-29 18:01:52 +0800

    Extended process_integers method to write squares of even numbers into double.txt and cubes of odd numbers into triple.txt while iterating through the list of integers.

--- square_cube_integers/integers_seperator.py
+++ square_cube_integers/integers_seperator.py
@@ -8,3 +8,10 @@ class IntegerSeperator:
                 numbers = []
                 for line in file.readlines():
                     numbers.append(int(line.strip()))
+
+        with open("double.txt", "w") as even_file, open("triple.txt", "w") as odd_file:
+            for num in numbers:
+                if num % 2 == 0:
+                    even_file.write(str(num ** 2) + "\n")
+                else:
+                    odd_file.write(str(num ** 3) + "\n")
